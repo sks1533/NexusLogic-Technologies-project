036cf85108065a4baae8b586fd938c2206ca2c0b
sks1533 <151610384+sks1533@users.noreply.github.com>
2025-01-12 15:19:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPageView1 from './components/LandingPageView1';
 import LandingPageView2 from './components/LandingPageView2';
-import MergedLandingPage from './components/MergedLandingPage'; 
+import MergedLandingPage from './components/MergedLandingPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<MergedLandingPage />} /> {/* Default route */}
         <Route path="/landing1" element={<LandingPageView1 />} />
         <Route path="/landing2" element={<LandingPageView2 />} />
-        <Route path="/landing3" element={<MergedLandingPage />} /> 
+        <Route path="/landing3" element={<MergedLandingPage />} />
       </Routes>
     </BrowserRouter>
   );
